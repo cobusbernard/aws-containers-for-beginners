cdd218aa39fcc0bc34bded871ef45638cd45ebcc
Cobus Bernard <cobusb@amazon.com>
2019-08-18 19:30:30 +0200

    Adding an ALB to expose the service

---
+++ infra/alb.tf
@@ -0,0 +1,66 @@
+# To expose the container to the internet, we will be using an Application Load-Balancer (ALB).
+# To be able to route traffic to our containers, we create a target group and regsiter the containers with it. 
+# We then have the ALB route traffic to the target group.
+# We require the following components for it:
+# 1. A target group
+# 2. An ALB
+# 3. A security group for the ALB to define how it may be accessed.
+
+resource "aws_security_group" "alb" {
+  name        = "alb-${var.alb_name}"
+  description = "ALB Security Group."
+  vpc_id      = "${module.vpc.vpc_id}"
+
+  tags = {
+    Name = "alb-${var.alb_name}"
+  }
+}
+
+resource "aws_security_group_rule" "alb_allow_http" {
+  type            = "ingress"
+  from_port       = 80
+  to_port         = 80
+  protocol        = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.alb.id}"
+}
+
+resource "aws_security_group_rule" "alb_allow_egress" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 65535
+  protocol        = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = "${aws_security_group.alb.id}"
+}
+
+resource "aws_alb_target_group" "webinar_service_target_group" {
+  name        = "${var.container_name}-target-group"
+  port        = "${var.container_port}"
+  protocol    = "HTTP"
+  vpc_id      = "${module.vpc.vpc_id}"
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_alb" "webinar_alb" {
+  name            = "${var.alb_name}"
+  subnets         = "${module.vpc.public_subnets}"
+  security_groups = ["${aws_security_group.alb.id}"]
+}
+
+resource "aws_alb_listener" "webinar_app" {
+  load_balancer_arn = "${aws_alb.webinar_alb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.webinar_service_target_group.arn}"
+    type             = "forward"
+  }
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -3,8 +3,18 @@ variable "container_name" {
   default = "webinar-web-app"
 }
 
+variable "container_port" {
+    type = "string"
+    default = "80"
+}
+
 variable "aws_region" {
   type    = "string"
   default = "eu-west-1"
+}
+
+variable "alb_name" {
+    type = "string"
+    default = "aws-webinar"
 }
 
